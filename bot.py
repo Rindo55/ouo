b4c2fe0d950706142351dd476ee5ceb681cbe33e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-14 12:48:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+import urllib
 from pyrogram import Client, filters, idle
 from config import *
 from uvloop import install
@@ -22,8 +23,8 @@ loop = asyncio.get_event_loop()
 
 @app.on_message(filters.private & filters.regex("http|https"))
 async def Bitly(bot, cmd: Message):
-  URL = cmd.text
-  api_url = f"https://ouo.io/api/jezWr0hG?s={URL}"
+  URL = urlencode(cmd.text)
+  api_url = f"https://ouo.io/api/jezWr0hG?s=URL"
   result = requests.post(api_url)
   nyaa_text = result.text   
   fuktext = "`" + nyaa_text + "`"
