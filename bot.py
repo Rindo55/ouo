5e62ed7f32c3f2fcadcd10320e57f8bcfb90053e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-15 20:03:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,9 +24,9 @@ loop = asyncio.get_event_loop()
 
 @app.on_message(filters.private & filters.regex("http|https"))
 async def Bitly(bot, cmd: Message):
-  URL = cmd.text
+  URL = str(cmd.text)
   api_url = f"http://ouo.io/api/jezWr0hG?s={URL}"
-  result = json.loads(requests.post(api_url))
+  result = requests.post(api_url)
   nai_text = result.text
   da_url = "https://da.gd/"
   url = nai_text
