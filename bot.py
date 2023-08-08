f4c3202f8f5852e66b6dd62740ff2200c9298931
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-08 09:02:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,9 +22,10 @@ app.start()
 
 loop = asyncio.get_event_loop()
 
-@app.on_message(filters.private & filters.command("sxslink") & filters.regex("http|https"))
+@app.on_message(filters.private & filters.command("sxs") & filters.regex("http|https"))
 async def Bitly(bot, cmd: Message):
-  xam = str(cmd.text)
+  fx = " ".join(message.command[1:])
+  xam = str(fx)
   da_url = "https://da.gd/"
   resp = f"https://sxslink.com/api?api=555d814b547a16c08f5c7cf99ae9bbe8edd34696&url={xam}&format=text"
   fukshare = requests.get(resp)
@@ -34,7 +35,20 @@ async def Bitly(bot, cmd: Message):
   tgshare = requests.get(xshare_url, params={"url": cshare})
   teleshare = tgshare.text.strip() 
   await cmd.reply_text(f"`{teleshare}`")  
-    
+
+@app.on_message(filters.private & filters.command("tn") & filters.regex("http|https"))
+async def Bitly(bot, cmd: Message):
+  fit = " ".join(message.command[1:])
+  xamx = str(fit)
+  da_urlx = "https://da.gd/"
+  respx = f"https://tnlinks.in/api?api=1458ad61946fd6f5b8a93161c9cfd94733813566&url={xamx}&format=text"
+  fuksharex = requests.get(respx)
+  tsharex = fuksharex.text
+  csharex = tsharex
+  xshare_urlx = f"{da_urlx}shorten"
+  tgsharex = requests.get(xshare_urlx, params={"url": csharex})
+  telesharex = tgsharex.text.strip() 
+  await cmd.reply_text(f"`{telesharex}`")
 async def start_bot():
   print("==================================")
   print("[INFO]: AutoAnimeBot Started Bot Successfully")
